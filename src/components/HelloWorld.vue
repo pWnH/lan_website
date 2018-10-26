ba55fce3e51b511eca3a5576b4fae2d76dae6dac
lsc <l.schnitzmeier@tkw.lan>
2018-10-26 15:18:13 +0200

    form auf home test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,17 @@
   <div class="hello">
     <h1>Hallo!</h1>
     <p>Wilkommen zur SUPER-DUPER-LAN!</p>
+        <form method="post" netlify>
+    <input type="hidden" name="form-name" value="games" />
+      <div class="cards-container">
+        <game-card v-for="game in games" :key="game.title" :game="game"/>
+      </div>
+      <br>
+      <label for="extra-games">Dein Lieblingsspiel nicht dabei? Trage hier weitere Vorschläge ein!</label>
+      <textarea id="extra-games" name="extra-games"/>
+      <br>
+      <button class="btn-primary">Auswahl abschicken</button>
+    </form>
   </div>
 </template>
 
@@ -10,6 +21,27 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      games: [
+        {
+          title: 'CounterStrike 1.6',
+          id: 'cs-16',
+          img: 'http://fc07.deviantart.net/fs70/f/2014/108/b/d/counter_strike_1_6_icon_by_dudekpro-d7f0049.png',
+        },
+        {
+          title: 'Counterstrike Global Offensive',
+          id: 'csgo',
+          img: 'https://vignette.wikia.nocookie.net/cswikia/images/0/0c/Csgo-payback-icon.png/revision/latest?cb=20141112151119',
+        },
+        {
+          title: 'Warcraft 3',
+          id: 'wc3',
+          img: 'http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-18/256/Warcraft-3-Frozen-Throne-1-icon.png',
+        },
+      ]
+    }
   }
 }
 </script>
