2e4829eb6c91273fa582fedbcaf1b3f015e5ac58
lsc <l.schnitzmeier@tkw.lan>
2018-10-26 14:46:22 +0200

    jetzt vielleicht?

--- src/views/Saved.vue
+++ src/views/Saved.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Supi! Deine Spielewahl wurde gespeichert!</h1>
+    <h1>Supi! Deine Spielewahl wurde gespeichert! {{test}}</h1>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            test: 'LOL!',
+        }
+    }
+}
+</script>
+
