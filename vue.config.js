ce69106403457787b8865d10503727e3546c13b2
lsc <l.schnitzmeier@tkw.lan>
2018-11-19 19:13:09 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,20 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
 
 module.exports = {
   configureWebpack: () => {
-    if (process.env.NODE_ENV !== 'production') return;
     return {
       plugins: [
         new PrerenderSPAPlugin(
           // Absolute path to compiled SPA
-          path.resolve(__dirname, 'dist'),
+          path.resolve(__dirname, "dist"),
           // List of routes to prerender
-          [ '/', '/games', '/games/'],
+          ["/", "/games", "/games/"],
           {
             // options
           }
-        ),
+        )
       ]
-    }
+    };
   }
-}
+};
